31dd70b5b3d98ed322483dc0ca9b187b055385c9
Aditya Kumar <aditya.11001kumar@gmail.com>
2025-01-06 00:00:38 +0530

    Create UserServlet.java

---
+++ UserServlet.java
@@ -0,0 +1,31 @@
+package com.smartirrigation;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("user.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String irrigationArea = request.getParameter("irrigationArea");
+
+        request.setAttribute("name", name);
+        request.setAttribute("email", email);
+        request.setAttribute("irrigationArea", irrigationArea);
+
+        request.getRequestDispatcher("profile.jsp").forward(request, response);
+    }
+}
